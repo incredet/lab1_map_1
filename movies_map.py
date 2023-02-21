6dd7ecf4edc554a5b8ba439ff4b2f57777523e24
Iryna Voitsitska <voitsitska.pn@ucu.edu.ua>
2023-02-21 09:47:51 +0200

    added name to the list of values, remains to add another layer

--- movies_map.py
+++ movies_map.py
@@ -1,9 +1,16 @@
 """ """
-from geopy.geocoders import Nominatim
-from geopy.geocoders import ArcGIS
 import re
+import folium
+from geopy.geocoders import Nominatim
 from typing import List
 from haversine import haversine
+import pandas as pd
+
+def create_dataframe(list_of_movies: list):
+    """ """
+    movies = pd.DataFrame(list_of_movies, columns=['name', 'lat', 'lot'])
+    return movies
+
 
 def find_coords(locs: str, year) -> list:
     """ """
@@ -13,6 +20,7 @@ def find_coords(locs: str, year) -> list:
     prev_line = ""
     for line in data_list:
         if str(year) in line:
+            name = re.findall(r'\"(.*?)\"|\'(.*?)\"', line)[0][0]
             line = re.sub("\(.*?\)|\{.*?\}|\".*?\"|\'.*?\"", "", line)
             if line != prev_line:
                 prev_line = line
@@ -20,7 +28,7 @@ def find_coords(locs: str, year) -> list:
                     geolocator = Nominatim(user_agent="map")
                     location = geolocator.geocode(line)
                     print((location.latitude, location.longitude))
-                    location_list += [(location.latitude, location.longitude)]
+                    location_list += [(name, location.latitude, location.longitude)]
                 except Exception:
                     print("LocationNotFound")
     return location_list
@@ -29,9 +37,22 @@ def find_coords(locs: str, year) -> list:
 def calculate_distance(coordinates: List[tuple], dest_lat, dest_lon) -> List[tuple]:
     """ """
     return sorted(coordinates, key = lambda coord:\
-         haversine((dest_lat, dest_lon), (coord[0], coord[1])))[:10]
+         haversine((dest_lat, dest_lon), (coord[1], coord[-1])))[:10]
 
+
+def create_map(coords):
+    """ """
+    movies_map = folium.Map()
+    movies_locs = folium.FeatureGroup(name = "nearest locations")
+    for coord in coords:
+        movies_locs.add_child(folium.Marker(location=[coord[1], coord[-1]],\
+             icon=folium.Icon(icon='cloud')))
+    movies_map.add_child(movies_locs)
+    movies_map.add_child(folium.LayerControl())
+    movies_map.save("Map_Marker_irynka2.html")
 
 
 values = find_coords("lab1_map_1/locations_ssv.list", 2013)
-print(calculate_distance(values, 31.7788242, 35.2257626))
+print(values)
+coords = calculate_distance(values, 31.7788242, 35.2257626)
+create_map(coords)
